71e7feeb99b30b0b6a30096e5f552200f5c5cf20
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-05-07 10:25:29 +0400

    Create parent and child classes with overridden methods

---
+++ solution14/project01/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Animal is Eating");
+            }
+            public virtual void sound()
+            {
+                Console.WriteLine("Animal make sound");
+            }
+            public virtual void sleep()
+            {
+                Console.WriteLine("Animal is sleep");
+            }
+        }
+        class Dog : Animal
+        {
+            public override void sound()
+            {
+                Console.WriteLine("Dog barks");
+            }
+            public override void sleep()
+            {
+                Console.WriteLine("Dog id sleep");
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.sleep();
+            dog.sound();
+        }
+    }
+}
